e1d94e03ea90bcda1c214d6e9dc391cf142d6bdd
Kevin Aspegren <kevin.aspegren@student.kyh.se>
2020-09-24 16:39:26 +0200

    Moved if-statement to check if sickness is null to isSick, added another if-statement to check if getTreatmentName equals sickness.

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -4,27 +4,22 @@ public class Patient {
 
     private String name;
     private String sickness;
-    private String medicine;
     private boolean sickBool = true;
 
-    Patient (String name) {
+    Patient (String name){
         this.name = name;
-        if (sickness == null) {
-            this.sickBool = false;
-        }
     }
 
-    Patient (String name, String sickness) {
+    Patient (String name, String sickness){
         this.name = name;
         this.sickness = sickness;
-        if (sickness == null) {
-            this.sickBool = false;
-        }
     }
 
     String takeMedication(Medicine medicine){
-        sickBool = false;
-        return this.medicine;
+        if(medicine.getTreatmentName().equals(sickness)){
+            sickness = null;
+        }
+        return sickness;
     }
 
     String getName(){
@@ -36,6 +31,9 @@ public class Patient {
     }
 
     Boolean isSick(){
+        if (sickness == null){
+            this.sickBool = false;
+        }
         return sickBool;
     }
 }
